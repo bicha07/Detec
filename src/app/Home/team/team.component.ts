9b5ae602209b514f462fffd678151e6083c06f0d
yessine_gargouri <yessine.gargouri@gmail.com>
2024-06-17 12:40:13 +0100

    version second

--- src/app/Home/team/team.component.ts
+++ src/app/Home/team/team.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
--- src/app/Service/single-s/single-s.component.ts
+++ src/app/Service/single-s/single-s.component.ts
@@ -1,15 +1,64 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../Home/footer/footer.component";
 import { NavbarComponent } from '../navbar/navbar.component';
-import { BannerComponent } from "../../Home/banner/banner.component";
+import {  AfterViewInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+declare var windowLoadInit: any; // Global declaration for existing init function
+declare var initializeToggleMenu: any; // Global declaration for toggle menu function
+ 
 
 @Component({
     selector: 'app-single-s',
     standalone: true,
     templateUrl: './single-s.component.html',
     styleUrl: './single-s.component.css',
-    imports: [FooterComponent, NavbarComponent, BannerComponent]
+    imports: [FooterComponent, NavbarComponent]
 })
 export class SingleSComponent {
 
-}
+    private routerSubscription: Subscription;
+  
+    constructor(private router: Router) {
+      // Listen to navigation end events
+      this.routerSubscription = this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          // Scroll to top of the viewport after each navigation
+          window.scrollTo(0, 0);
+          // Reinitialize jQuery functionalities after each navigation
+          this.reinitializeJQueryFunctions();
+        }
+      });
+    }
+  
+    ngAfterViewInit() {
+      // Initialize any animations after the view is initially loaded
+      this.initAnimations();
+      // Ensure that jQuery functionalities are initialized after the initial view is loaded
+      this.reinitializeJQueryFunctions();
+    }
+  
+    ngOnDestroy() {
+      // Unsubscribe to prevent memory leak
+      if (this.routerSubscription) {
+        this.routerSubscription.unsubscribe();
+      }
+    }
+  
+
+  
+    private initAnimations() {
+      // Call the global animation initialization function if it exists
+      if (typeof windowLoadInit === 'function') {
+        windowLoadInit();
+      }
+    }
+  
+    private reinitializeJQueryFunctions() {
+      // Call the global function to initialize jQuery toggle menu if it exists
+      if (typeof initializeToggleMenu === 'function') {
+        initializeToggleMenu();
+      }
+    }
+  }
